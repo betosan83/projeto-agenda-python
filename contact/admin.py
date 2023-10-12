b7e29a917dbf1156d6df12fefdd44e76cf417dbf
Roberto <beto.boscolo@gmail.com>
2023-10-12 15:18:14 -0300

    Novos models de categoria, contatos e owner.

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
+from contact import models
+
+
 # Register your models here.
+
+
+@admin.register(models.Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = 'first_name', 'last_name', 'phone',
+    ordering = 'first_name',
+    search_fields = 'first_name',
+
+
+@admin.register(models.Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = 'name',
