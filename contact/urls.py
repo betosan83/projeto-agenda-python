0e075d388882eaa86f4a9511a88e934ade0b9574
Roberto <beto.boscolo@gmail.com>
2023-10-15 06:51:22 -0300

    Primeira versão da ferramenta de busca da agenda.

--- contact/urls.py
+++ contact/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
+
 from contact import views
 
 app_name = 'contact'
 
 urlpatterns = [
+    path('<int:contact_id>/', views.contact, name='contact'),
+    path('search/', views.search, name='search'),  # type:ignore
     path('', views.index, name='index'),
 ]
